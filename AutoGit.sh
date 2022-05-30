c35efaacecd0f6d39188754091289dfe00ed1634
yello12 <brickbeacon123@gmail.com>
2022-05-30 12:22:39 -0700

    arams stuff

---
+++ AutoGit.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Use this by navigating to the repo and running it as a shell command
+# Only usefult o people with admin priveledges (idk how to spell this word) lol
+
+echo "Starting automatic Git commit and push; Type commit msg here: "
+message=$1
+
+if [ ! -z "$1" ]
+then
+   git add * 
+   git commit -m "$message"
+   git push origin main
+else
+   echo  "Commit message is not provided you fool!"
+fi
