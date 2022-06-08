52f2a1cb457f5bea4611a24fb5867933ffd7e3ba
yello12 <brickbeacon123@gmail.com>
2022-06-08 10:53:44 -0700

    last minute

--- src/script.js
+++ src/script.js
@@ -13,25 +13,25 @@ function toggleDropdown() {
 }
 
 
-const readMore = document.getElementById("read-more");
-$(document.body).on('touchmove', () => {resizeReadMore()});
-$(document).ready(() => {resizeReadMore()});
-$(window).scroll(() => {resizeReadMore()});
-$(window).resize(() => {resizeReadMore()});
+// const readMore = document.getElementById("read-more");
+// $(document.body).on('touchmove', () => {resizeReadMore()});
+// $(document).ready(() => {resizeReadMore()});
+// $(window).scroll(() => {resizeReadMore()});
+// $(window).resize(() => {resizeReadMore()});
 
-function resizeReadMore() {
-   const a = document.documentElement.scrollTop;
-   const b = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-   // Im actually fucking malding rn. What fucking nerd JS developer calls the root node a documentELement. 
-   // I thought that was a placeholder for an actual document element like 'body' or 'footer'. What an absolute fucking nonce. 
-   // End your career IMMEDIATELY and give me $500 JS coins or I will uhh... uh.. auauahhhhhh
-   const c = a / b;
-   console.log(a);
-	if (b === 0 || c > 0.4) {
-      // readMore.style.display = "none";
-      readMore.style.opacity = 0;
-   } else {
-      // readMore.style.display = "inherit";
-      readMore.style.opacity = 1;
-   }
-}
+// function resizeReadMore() {
+//    const a = document.documentElement.scrollTop;
+//    const b = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//    // Im actually fucking malding rn. What fucking nerd JS developer calls the root node a documentELement. 
+//    // I thought that was a placeholder for an actual document element like 'body' or 'footer'. What an absolute fucking nonce. 
+//    // End your career IMMEDIATELY and give me $500 JS coins or I will uhh... uh.. auauahhhhhh
+//    const c = a / b;
+//    console.log(a);
+// 	if (b === 0 || c > 0.4) {
+//       // readMore.style.display = "none";
+//       readMore.style.opacity = 0;
+//    } else {
+//       // readMore.style.display = "inherit";
+//       readMore.style.opacity = 1;
+//    }
+// }
